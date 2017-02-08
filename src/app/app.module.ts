cb8921153f3b22876a3bfe9ec3a685013385cebe
AsheToll <tolenashley7@gmail.com>
2017-02-08 12:50:04 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-/*import { AngularFireModule } from 'angularfire2';*/
 import { YumHack } from './app.component';
 import { MenuPage } from '../pages/menu/menu';
 import { FeedbackPage } from '../pages/feedback/feedback';
@@ -10,14 +9,6 @@ import { ChickfilaPage } from '../pages/chickfila/chickfila';
 import { ChipotlePage } from '../pages/chipotle/chipotle';
 import { ShakeshackPage } from '../pages/shakeshack/shakeshack';
 import { StarbucksPage } from '../pages/starbucks/starbucks';
-
-/* export const firebaseConfig = {
-    apiKey: "AIzaSyDd6h165WKpMU2IcR4YqqLo8sbRVzXyZic",
-    authDomain: "yumhack-c5653.firebaseapp.com",
-    databaseURL: "https://yumhack-c5653.firebaseio.com",
-    storageBucket: "yumhack-c5653.appspot.com",
-    messagingSenderId: "483133115561"
-}; */
 
 @NgModule({
   declarations: [
@@ -32,8 +23,7 @@ import { StarbucksPage } from '../pages/starbucks/starbucks';
     StarbucksPage
   ],
   imports: [
-      IonicModule.forRoot(YumHack, { tabsPlacement: 'top' })/*,
-      AngularFireModule.initializeApp(firebaseConfig)*/
+      IonicModule.forRoot(YumHack, { tabsPlacement: 'top' })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
